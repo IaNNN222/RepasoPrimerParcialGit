fa7313b84d31c7ce0b12faecd1846a6b40e1bc89
Ian Galindo <171846997+IaNNN222@users.noreply.github.com>
2025-02-21 10:54:49 -0600

    Add files via upload
    Archivo principal

---
+++ c++main/LabDeRepaso/LabDeRepaso/LabDeRepaso.cpp
@@ -0,0 +1,10 @@
+// LabDeRepaso.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#
+//Grupo de programacion
+
+//Emerson Reyes
+//Gerson Perez
+//Cristhoper hwang
+//Ian Galindo
