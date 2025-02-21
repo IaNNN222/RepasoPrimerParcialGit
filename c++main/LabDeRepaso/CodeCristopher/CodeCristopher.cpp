60920650ecd324ca8ee077a6b92f490e4316bced
CristopherHwang <pool123hwang@gmail.com>
2025-02-21 12:00:48 -0600

    actualizacion

--- c++main/LabDeRepaso/CodeCristopher/CodeCristopher.cpp
+++ c++main/LabDeRepaso/CodeCristopher/CodeCristopher.cpp
@@ -1,19 +1,41 @@
-// CodeCristopher.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
-
+#include <string>
 #include <iostream>
+using namespace std;
+
+class Pokemon {
+private: 
+    string nombre;
+    int hp;
+    int defensa;
+    int ataque;
+    int velocidad; 
+public: 
+    Pokemon(string _nombre, int _hp, int _defensa, int _ataque, int _velocidad) {
+        nombre = _nombre;
+        hp = _hp;
+        defensa = _defensa; 
+        ataque = _ataque;
+        velocidad = _velocidad;
+    }
+
+    void evolucion() {
+        cout << nombre << " Ha evolucionado correctamente" << endl;
+    }
+
+};
+
+class Agua : Pokemon {
+private:
+    string debilidad;
+    string fortaleza;
+public: 
+    Agua(string _nombre, int _hp, int _defensa, int _ataque, int _velocidad, string _debilidad, string _fortaleza) : Pokemon(_nombre,_hp,_defensa,_ataque,_velocidad), debilidad(_debilidad), fortaleza(_fortaleza) {
+        debilidad = _debilidad;
+        fortaleza = _fortaleza;
+    }
+};
 
 int main()
 {
     std::cout << "Hello World!\n";
 }
-
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
