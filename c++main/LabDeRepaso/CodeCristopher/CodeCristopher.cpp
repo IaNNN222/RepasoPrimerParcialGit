f4e567ba4786b12710be14711d0ab711f9be0af4
CristopherHwang <pool123hwang@gmail.com>
2025-02-21 19:23:23 -0600

    actualizacion 2

--- c++main/LabDeRepaso/CodeCristopher/CodeCristopher.cpp
+++ c++main/LabDeRepaso/CodeCristopher/CodeCristopher.cpp
@@ -24,18 +24,65 @@ public:
 
 };
 
-class Agua : Pokemon {
+class Agua : public Pokemon {
 private:
-    string debilidad;
-    string fortaleza;
+    string ataqueA;
 public: 
-    Agua(string _nombre, int _hp, int _defensa, int _ataque, int _velocidad, string _debilidad, string _fortaleza) : Pokemon(_nombre,_hp,_defensa,_ataque,_velocidad), debilidad(_debilidad), fortaleza(_fortaleza) {
-        debilidad = _debilidad;
-        fortaleza = _fortaleza;
+    Agua(string _nombre, int _hp, int _defensa, int _ataque, int _velocidad, string _ataqueA) : Pokemon(_nombre,_hp,_defensa,_ataque,_velocidad), ataqueA(_ataqueA) {
+        ataqueA = _ataqueA;
     }
+
+    void movimientoA() {
+        cout << "Ha realizado: " << ataqueA << endl;
+    }
+
+};
+
+class Fuego : public Pokemon {
+private: 
+    string ataqueF;
+public:
+    Fuego(string _nombre, int _hp, int _defensa, int _ataque, int _velocidad, string _ataqueF) : Pokemon(_nombre, _hp, _defensa, _ataque, _velocidad), ataqueF(_ataqueF) {
+        ataqueF = _ataqueF;
+    }
+
+    void movimientoF() {
+        cout << "Ha realizado: " << ataqueF << endl;
+    }
+
+};
+
+class Electrico : public Pokemon {
+private: 
+    string ataqueE;
+public: 
+    Electrico(string _nombre, int _hp, int _defensa, int _ataque, int _velocidad, string _ataqueE) : Pokemon(_nombre, _hp, _defensa, _ataque, _velocidad), ataqueE(_ataqueE) {
+        ataqueE = _ataqueE;
+    }
+
+    void movimientoE() {
+        cout << "Ha realizado: " << ataqueE << endl;
+    }
+
 };
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Electrico e1("Pikachu", 25, 10, 20, 30, "Impactrueno");
+    Fuego f1("Ponyta", 28, 15, 25, 12, "Llamarada");
+    Agua a1("Lapras", 40, 35, 10, 10, "Hidrobomba");
+
+    cout << "Tu pokemon: " << endl;
+    e1.movimientoE();
+    cout << "Tu pokemon ha: " << endl;
+    e1.evolucion();
+
+    cout << "Tu pokemon: " << endl;
+    a1.movimientoA();
+    cout << "Tu pokemon ha: " << endl;
+
+    cout << "Tu pokemon: " << endl;
+    f1.movimientoF();
+    cout << "Tu pokemon ha: " << endl;
+
 }
